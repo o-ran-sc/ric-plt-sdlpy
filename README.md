fbbc4c32254a2e6255883d4faa8a2fc006959d6e
Timo Tietavainen <timo.tietavainen@nokia.com>
2019-11-27 11:50:01 +0200

    Add implementation of SDL in python
    Added implementation for the SDL API functions:
    * Functions to set, get and remove synchronously key-values from
      SDL storage.
    * Functions to set, get and remove synchronously group members from
      SDL storage.
    * Functions to acquire, manipulate and release a lock.
    
    Added also simple examples how to use SDL API functions.
    
    Added configuration file for the tox tool to run unittests.
    
    Added configuration file for the tox tool to generate documents.
    
    Change-Id: I1f12879f725d903397ee8f9e788edf7890db381d
    Signed-off-by: Timo Tietavainen <timo.tietavainen@nokia.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Python shareddatalayer library
+# Python Shared Data Layer (SDL) library in RAN Intelligent Controller (RIC)
+
+Please see the [README.md](./ricsdl-package/README.md) in `ricsdl-package` directory.
--- ricsdl-package/ricsdl/exceptions.py
+++ ricsdl-package/ricsdl/exceptions.py
@@ -13,7 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"Exceptions raised by the shareddatalayer."
+#
+# This source code is part of the near-RT RIC (RAN Intelligent Controller)
+# platform project (RICP).
+#
+
+
+"Exceptions raised by the Shared Data Layer (SDL)."
+
 
 class SdlTypeError(TypeError):
     """
@@ -23,30 +30,34 @@ class SdlTypeError(TypeError):
     """
     pass
 
+
 class SdlException(Exception):
-    """Base exception class for shareddatalayer exceptions."""
+    """Base exception class for Shared Data Layer (SDL) exceptions."""
     pass
+
 
 class NotConnected(SdlException):
     """
-    Exception for not being connected to the database backend.
-    Shareddatalayer is not connected to the backend data storage and therefore could not deliver the
-    request to the backend data storage. Data in the backend data storage has not been altered.
+    Exception for SDL not being connected to the database backend.
+    SDL is not connected to the backend data storage and therefore could not deliver the request
+    to the backend data storage. Data in the backend data storage has not been altered.
     Client is advised to try the operation again later.
     """
     pass
 
+
 class BackendError(SdlException):
     """
-    Exception for request processing failure.
+    Exception for request processing failure in SDL database backend.
     In case of a write type request, data in the backend data storage may or may not have been
     altered. Client is advised to try the operation again later.
     """
     pass
 
+
 class RejectedByBackend(SdlException):
     """
-    Exception for shareddatalayer rejecting the request.
+    Exception for SDL database backend rejecting the request.
     Backend data storage rejected the request. In case of a write type request, data in the backend
     data storage may or may not have been altered. It is likely that the same request will fail
     repeatedly. It is advised to investigate the exact reason for the failure from the logs.
