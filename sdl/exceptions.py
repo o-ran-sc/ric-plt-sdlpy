f32b1d4cf2e80962adf6d8170b296a51084a63dd
Timo Tietavainen <timo.tietavainen@nokia.com>
2019-11-27 11:50:01 +0200

    Add implementation of SDL in python
    Added implementation for the SDL API functions:
    * Functions to set, get and remove synchronously key-values from
      SDL storage.
    * Functions to set, get and remove synchronously group members from
      SDL storage.
    * Functions to acquire, manipulate and release a lock.
    Added also simple examples how to use SDL API functions.
    
    Signed-off-by: Timo Tietavainen <timo.tietavainen@nokia.com>
    Change-Id: I1f12879f725d903397ee8f9e788edf7890db381d
    Signed-off-by: Timo Tietavainen <timo.tietavainen@nokia.com>

--- sdl/exceptions.py
+++ sdl/exceptions.py
@@ -12,8 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# This source code is part of the near-RT RIC (RAN Intelligent Controller)
+# platform project (RICP).
+
 
 "Exceptions raised by the shareddatalayer."
+
 
 class SdlTypeError(TypeError):
     """
